ce0140f04db17383a3385b3c4ad797cf4c24d057
Bradley Stannard <bstannard@petsathome.co.uk>
2023-07-01 15:48:35 +0100

    Adding a homepage

--- example/terraform/bucket.tf
+++
@@ -1,6 +0,0 @@
-resource "google_storage_bucket" "gcs-website" {
-  name                        = "gcs-website"
-  location                    = var.location
-  project                     = var.project
-  uniform_bucket_level_access = true
-}
--- example/terraform/serviceaccount.tf
+++
@@ -1,6 +0,0 @@
-resource "google_service_account" "gcs-website" {
-  display_name = "WID GCS Webserver"
-  account_id   = "wid-gcs-website"
-  description  = "Service account used in GKE for gcs-webserver"
-  project      = var.project
-}
